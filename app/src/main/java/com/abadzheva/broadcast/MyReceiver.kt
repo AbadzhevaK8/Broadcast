cc348af83ecfbfcb062e1f209ef9be9e38ddc2b3
k8abadzheva <make.me.loco@gmail.com>
2024-12-01 23:36:06 +0300

    Initial commit

---
+++ app/src/main/java/com/abadzheva/broadcast/MyReceiver.kt
@@ -0,0 +1,20 @@
+package com.abadzheva.broadcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyReceiver : BroadcastReceiver() {
+    override fun onReceive(
+        context: Context?,
+        intent: Intent?,
+    ) {
+        val action = intent?.action
+        when (action) {
+            Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
+                Toast.makeText(context, "Airplane mode changed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
